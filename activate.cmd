5857f5811a6de1740be50ca8551dcd50e8f989b2
Jarkko Piiroinen <jmp@git.mail.kapsi.fi>
2019-09-29 15:17:11 +0300

    Detect script directory correctly

--- activate.cmd
+++ activate.cmd
@@ -1,5 +1,6 @@
-@set CMAKE_BIN=%CD%\cmake\bin
-@set MINGW_BIN=%CD%\mingw\bin
-@set PYTHON_BIN=%CD%\python
-@set HG_BIN=%CD%\mercurial
+@for %%i in ("%~dp0.") do @set SCRIPT_DIR=%%~fi
+@set CMAKE_BIN=%SCRIPT_DIR%\cmake\bin
+@set MINGW_BIN=%SCRIPT_DIR%\mingw\bin
+@set PYTHON_BIN=%SCRIPT_DIR%\python
+@set HG_BIN=%SCRIPT_DIR%\mercurial
 @set PATH=%CMAKE_BIN%;%MINGW_BIN%;%PYTHON_BIN%;%HG_BIN%;%PATH%
